2780b5d9b24b3e0aa85c1e9b0222eee3b102de85
mrimmoaee <mrimmoaee@gmail.com>
2022-11-10 10:15:47 +0600

    first commit

---
+++ Ch02/issue.js
@@ -0,0 +1,10 @@
+const queue =
+  '[{"id":"394","title":"Eat your veggies"},{"id":"378","title":"Every step counts"},{"id":"406","title":"Giving Back"}]';
+
+// Enter code to destringify the `queue` variable here:
+const queueData = JSON.parse(queue);
+
+const itemValue = '{"id":"406","title":"Giving Back"}';
+
+// Enter code to destringify the `item` variable here:
+const ItemData = JSON.parse(itemValue);
